d03ed3d0c1237e512fb3428caefaa461927af524
Skip <skip@usecurling.com>
2026-04-29 20:20:24 +0000

    Fix: Wrap page with AuthProvider to resolve crash

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Toaster } from '@/components/ui/toaster'
 import { Toaster as Sonner } from '@/components/ui/sonner'
 import { TooltipProvider } from '@/components/ui/tooltip'
 import { AppProvider } from '@/context/app-context'
+import { AuthProvider } from '@/hooks/use-auth'
 import Layout from '@/components/Layout'
 import Login from '@/pages/Login'
 import MyList from '@/pages/MyList'
@@ -12,21 +13,23 @@ import NotFound from '@/pages/NotFound'
 
 const App = () => (
   <BrowserRouter future={{ v7_startTransition: false, v7_relativeSplatPath: false }}>
-    <AppProvider>
-      <TooltipProvider>
-        <Toaster />
-        <Sonner />
-        <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Login />} />
-            <Route path="/minha-lista" element={<MyList />} />
-            <Route path="/admin" element={<AdminPanel />} />
-            <Route path="/banco-engenheiros" element={<EngineersDb />} />
-          </Route>
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </TooltipProvider>
-    </AppProvider>
+    <AuthProvider>
+      <AppProvider>
+        <TooltipProvider>
+          <Toaster />
+          <Sonner />
+          <Routes>
+            <Route element={<Layout />}>
+              <Route path="/" element={<Login />} />
+              <Route path="/minha-lista" element={<MyList />} />
+              <Route path="/admin" element={<AdminPanel />} />
+              <Route path="/banco-engenheiros" element={<EngineersDb />} />
+            </Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </TooltipProvider>
+      </AppProvider>
+    </AuthProvider>
   </BrowserRouter>
 )
 
